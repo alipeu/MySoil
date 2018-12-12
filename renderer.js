97561033d07622c29732aa1d8508c2d6d0ee23de
alipeu <alifhilmirc@gmail.com>
2018-12-13 02:12:44 +0700

    working login and signup, firebase integration

--- renderer.js
+++ renderer.js
@@ -1,3 +1,44 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBYhZOCw_Ma-0SCHAf3geJgwmTh13YuVd0",
+    authDomain: "mysoil-29827.firebaseapp.com",
+    databaseURL: "https://mysoil-29827.firebaseio.com",
+    projectId: "mysoil-29827",
+    storageBucket: "mysoil-29827.appspot.com",
+    messagingSenderId: "998601839541"
+};
+firebase.initializeApp(config);
+
+var logInBtn = document.getElementById('logInBtn');
+var signUpBtn = document.getElementById('signUpBtn');
+
+logInBtn.addEventListener('click', function() {
+    var emailField = document.getElementById('email').value;
+    var passwordField = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(emailField, passwordField).then(function() {
+        document.location.href = 'index.html';
+    }).catch(function(error) {
+        if(error != null) {
+            console.log(error.message);
+            alert("Incorrect Email or Password!");
+            return;
+        }
+    });
+});
+
+signUpBtn.addEventListener('click', function() {
+    var emailField = document.getElementById('email').value;
+    var passwordField = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(emailField, passwordField).then(function() {
+        alert('Signed up succesfully!');
+    }).catch(function(error) {
+        if(error != null) {
+            console.log(error.message);
+            alert("Email has already been taken!");
+            return;
+        }
+    });
+});
+
---
+++ ssqicount.js
@@ -0,0 +1,19 @@
+var hitungBtn = document.getElementById('hitungBtn');
+var pesatu = document.getElementById('pesatu').value;
+/*var p2 = document.getElementById('p2').value;
+var p3 = document.getElementById('p3').value;
+var p4 = document.getElementById('p4').value;
+var p5 = document.getElementById('p5').value;
+var p6 = document.getElementById('p6').value;
+var p7 = document.getElementById('p7').value;
+var p8 = document.getElementById('p8').value;
+var p9 = document.getElementById('p9').value;
+var p10 = document.getElementById('p10').value;
+var p11 = document.getElementById('p11').value;
+var p12 = document.getElementById('p12').value;
+var p13 = document.getElementById('p13').value;
+var p14 = document.getElementById('p14').value;*/
+
+hitungBtn.addEventListener('click', function() {
+    console.log(pesatu);
+});
